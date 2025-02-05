330698f5ed06f865e595a83b6f9b6f5787683c04
rahulbarmann <itsrahulbarman1@gmail.com>
2025-02-05 18:19:06 +0530

    refactor: update linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -12,13 +12,25 @@ export default defineConfig(
   },
   {
     rules: {
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'error',
-      'react-refresh/only-export-components': 'off',
-
-      '@typescript-eslint/no-explicit-any': 'warn',
-
-      'unicorn/prefer-module': 'off',
+      'simple-import-sort/exports': 'off',
+      'unicorn/new-for-builtins': 'off',
+      '@typescript-eslint/array-type': 'off',
+      '@stylistic/multiline-ternary': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      '@stylistic/comma-dangle': 'off',
+      '@stylistic/arrow-parens': 'off',
+      '@stylistic/jsx-one-expression-per-line': 'off',
+      'antfu/consistent-list-newline': 'off',
+      '@stylistic/jsx-closing-bracket-location': 'off',
+      '@stylistic/brace-style': 'off',
+      'antfu/if-newline': 'off',
+      '@stylistic/operator-linebreak': 'off',
+      'arrow-body-style': 'off',
+      'unicorn/prefer-number-properties': 'off',
+      '@stylistic/member-delimiter-style': 'off',
+      'antfu/top-level-function': 'off',
+      'simple-import-sort/imports': 'off',
+      '@stylistic/semi': 'off',
     },
-  },
+  }
 )
